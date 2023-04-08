8fda896cd3fedec3a996dd1b4e943deaf0c53f2e
StevenKirke <SFX.Montage@gmail.com>
2023-04-08 22:45:24 +0300

    first commit

---
+++ MarsRovers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarsRovers
+//
+//  Created by Steven Kirke on 08.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
