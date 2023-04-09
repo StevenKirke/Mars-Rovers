b93e8f19616b3c2b46ee67adda6d44414068d5ca
StevenKirke <SFX.Montage@gmail.com>
2023-04-09 18:59:09 +0300

    Add LaunchScreen

---
+++ MarsRovers/Extensions/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  Fonts.swift
+//  MarsRovers
+//
+//  Created by Steven Kirke on 09.04.2023.
+//
+
+import Foundation
+
+
+struct Fonts {
+    let nasa: String = "Nasa.ttf"
+}
---
+++ MarsRovers/View/ContentView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift.swift
+//  MarsRovers
+//
+//  Created by Steven Kirke on 08.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("NASA")
+            .font(Font.custom("nasa", size: 18))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
